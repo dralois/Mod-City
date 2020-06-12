e1d39ab67d1667c1977c8e3970d40053c573e399
Jakob Goes <jakob.goes@tum.de>
2020-06-12 23:50:04 +0200

    Mod Manager

---
+++ Assets/Scripts/Mods/TestMod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMod : ModBase
+{
+    public override float ModifyJumpHeight(float org)
+    {
+        if (version == 0)
+            return org * 2;
+        else
+            return org * 3;
+    }
+}
