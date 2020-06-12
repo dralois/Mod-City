79ad3701ae4b7d7d5de9a25c938428e36328f69e
VFelix <ge38xes@mytum.de>
2020-06-12 22:29:19 +0200

    UI added first main menu
    Also in this commit: added texmeshpro for UI

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevelByIndex(int levelIndex)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevelByName(string levelName)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    public void printHi()
+    {
+        Debug.Log("Hi");
+    }
+}
