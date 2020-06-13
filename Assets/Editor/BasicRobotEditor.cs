1e0675133af804bd0406220ee429dbc95d15d9d5
Jakob Goes <jakob.goes@tum.de>
2020-06-13 12:16:01 +0200

    Basic Robot

---
+++ Assets/Editor/BasicRobotEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(BasicRobot))]
+[CanEditMultipleObjects]
+public class BasicRobotEditor : Editor
+{
+    BasicRobot robot;
+
+    void OnEnable()
+    {
+        robot = (BasicRobot) target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+
+    private void OnSceneGUI()
+    {
+        Handles.color = Color.green;
+        robot.pos1 = Handles.FreeMoveHandle(robot.pos1, Quaternion.identity, 0.1F, Vector3.one, Handles.CircleHandleCap);
+        robot.pos2 = Handles.FreeMoveHandle(robot.pos2, Quaternion.identity, 0.1F, Vector3.one, Handles.CircleHandleCap);
+        Handles.DrawDottedLine(robot.pos1,robot.pos2, 2F);
+    }
+}
