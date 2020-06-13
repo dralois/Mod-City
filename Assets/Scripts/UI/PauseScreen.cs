717fb43592860cd7b05308e632b1130e662ff2f0
VFelix <ge38xes@mytum.de>
2020-06-13 21:44:24 +0200

    Added UI prefab for in game UI

---
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * written by Felix Völk
+ * 
+ * Script for UI in the playing game, changes Timescale, displays a HUD when slowed and provides a slider
+ * 
+ */
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timepause;
+    private bool paused = false;
+    public GameObject canvas;
+    private InputHandler inputHandler;
+    void Start()
+    {
+        inputHandler = new InputHandler();
+        inputHandler.Player.Cancel.performed += Cancel_performed;
+        inputHandler.Player.Cancel.Enable();
+        canvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
+    }
+    private void Cancel_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+            Pause();
+
+    }
+
+  
+
+    public void Pause()
+    {
+        if (!paused)
+        {
+            Time.timeScale = timepause;
+            canvas.SetActive(true);
+            paused = true;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            canvas.SetActive(false);
+            paused = false;
+        }
+    }
+
+    public void LoadLevelByIndex(int levelIndex)
+    {
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    public void LoadLevelByName(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
