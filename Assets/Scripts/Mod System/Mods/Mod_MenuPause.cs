318c1e0748fd87b7fc0d6a63b307e3f29a62e530
VFelix <ge38xes@mytum.de>
2020-06-14 01:12:40 +0200

    Added pause mod
    also added copy of first level for testing

---
+++ Assets/Scripts/Mod System/Mods/Mod_MenuPause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Mod", menuName = "Mods/Real Pause Menu")]
+public class Mod_MenuPause : IModObject
+{
+    protected override void AwakeInternal()
+    {
+        (Modable as PauseScreen).timepause = 0.0003f;
+    }
+
+    protected override void DestroyInternal()
+    {
+    }
+
+    protected override void OnDisableInternal()
+    {
+    }
+
+    protected override void OnEnableInternal()
+    {
+    }
+
+    protected override void UpdateInternal()
+    {
+    }
+}
