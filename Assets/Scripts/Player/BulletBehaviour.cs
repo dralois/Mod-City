971a4a57d8e0fe3613d2e340fde95fea88618a43
Jakob Goes <jakob.goes@tum.de>
2020-06-14 00:47:03 +0200

    Fixed projectiles, particles

--- Assets/Scripts/Player/BulletBehaviour.cs
+++ Assets/Scripts/Player/BulletBehaviour.cs
@@ -6,19 +6,24 @@ public class BulletBehaviour : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+
     [SerializeField]
     private float lifetime;
+
+    public GameObject hitPrefab;
 
     void Update()
     {
         lifetime -= Time.deltaTime;
         if (lifetime < 0)
-        {
             Destroy(gameObject);
-        }
         else
-        {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
-        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(hitPrefab, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
