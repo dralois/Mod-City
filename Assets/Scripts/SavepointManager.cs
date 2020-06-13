619c49687c2a1af18490fa131eb0649b73f50364
Jakob Goes <jakob.goes@tum.de>
2020-06-13 17:01:46 +0200

    Savepoints

---
+++ Assets/Scripts/SavepointManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavepointManager : Singleton<SavepointManager>
+{
+    public Savepoint inital;
+    public Savepoint lastSave;
+
+    private void Start()
+    {
+        lastSave = inital;
+        DontDestroyOnLoad(this);
+    }
+
+    public void Save(Savepoint s)
+    {
+        lastSave.sprite.color = Color.green;
+        s.sprite.color = Color.white;
+        lastSave = s;
+    }
+
+    public void OnReset()
+    {
+
+    }
+}
