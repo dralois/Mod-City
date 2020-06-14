ad7425afb20e7c4050306188139b114b66522516
VFelix <ge38xes@mytum.de>
2020-06-14 11:56:54 +0200

    Added winable mod

---
+++ Assets/Scripts/Mod System/Mods/Mod_Winable.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+[CreateAssetMenu(fileName = "New Mod", menuName = "Mods/Winable Game")]
+public class Mod_Winable : IModObject
+{
+    protected override void AwakeInternal()
+    {
+        (Modable as WinObject).isWinable = true;
+    }
+
+    protected override void DestroyInternal()
+    {
+    }
+
+    protected override void OnDisableInternal()
+    {
+    }
+
+    protected override void OnEnableInternal()
+    {
+    }
+
+    protected override void UpdateInternal()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
