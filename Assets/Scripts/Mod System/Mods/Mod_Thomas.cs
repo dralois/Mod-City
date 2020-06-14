972190ea62506d848deeef8ff5aa352787eae9d3
Jakob Goes <jakob.goes@tum.de>
2020-06-14 13:57:47 +0200

    Thomas mod

---
+++ Assets/Scripts/Mod System/Mods/Mod_Thomas.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "New Mod", menuName = "Mods/Thomas Mod")]
+public class Mod_Thomas : IModObject
+{
+    public GameObject thomas;
+
+	protected override void AwakeInternal()
+	{
+	}
+
+	protected override void OnEnableInternal()
+	{
+        Instantiate(thomas);
+	}
+
+	protected override void UpdateInternal()
+	{
+
+	}
+
+	protected override void OnDisableInternal()
+	{
+
+	}
+
+	protected override void DestroyInternal()
+	{
+	}
+}
