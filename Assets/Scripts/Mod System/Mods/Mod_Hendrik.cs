46500a5e8f9f4f65a5ca0fd4241c7502ebdac714
Jakob Goes <jakob.goes@tum.de>
2020-06-14 10:53:55 +0200

    Hendrik Mod

---
+++ Assets/Scripts/Mod System/Mods/Mod_Hendrik.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "New Mod", menuName = "Mods/Hendrik Mod")]
+public class Mod_Hendrik : IModObject
+{
+
+	protected override void AwakeInternal()
+	{
+	}
+
+	protected override void OnEnableInternal()
+	{
+        SpriteResolver[] r = (Modable as PlayerBehaviour).GetComponentsInChildren<SpriteResolver>();
+        foreach (SpriteResolver s in r)
+            s.SetCategoryAndLabel(s.GetCategory(), "Hendrik");
+	}
+
+	protected override void UpdateInternal()
+	{
+
+	}
+
+	protected override void OnDisableInternal()
+	{
+
+	}
+
+	protected override void DestroyInternal()
+	{
+	}
+}
