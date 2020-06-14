51f597703887516cba21c6ac055e307c2206140e
VFelix <ge38xes@mytum.de>
2020-06-14 04:41:43 +0200

    World war Z

--- Assets/Scripts/Mod System/ModTester.cs
+++ Assets/Scripts/Mod System/ModTester.cs
@@ -1,5 +1,25 @@
 ﻿public class ModTester : IModable
 {
+	protected override void AwakeInternal()
+	{
+	}
+
+	protected override void OnDestroyInternal()
+	{
+	}
+
+	protected override void OnDisableInternal()
+	{
+	}
+
+	protected override void OnEnableInternal()
+	{
+	}
+
+	protected override void UpdateInternal()
+	{
+	}
+
 	private void Start()
 	{
 		foreach (var mod in _mods)
